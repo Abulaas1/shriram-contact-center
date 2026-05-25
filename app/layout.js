ca8f134bbb4fb43dcc2c9ff32312eaf3193777c6
Abulaas1 <106108699+Abulaas1@users.noreply.github.com>
2026-05-26 02:25:04 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,2 @@
-export const metadata = {
-  title: "Shriram Contact Center",
-  description: "Financial Services Website"
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+export const metadata={title:'Shriram Contact Center'};
+export default function RootLayout({children}){return <html><body>{children}</body></html>}
